0f15df653421327504c9eed76f9d6f3821b1dd39
beki255 <bereket2553@gmail.com>
2026-05-13 20:52:03 +0300

    changed

--- src/train_model.py
+++ src/train_model.py
@@ -86,8 +86,6 @@ def train_models():
     print("\nConfusion Matrix:")
     cm = confusion_matrix(y_test, y_pred_best)
     print(cm)
-    
-    
     models_dir = os.path.join(PROJECT_ROOT, 'models')
     os.makedirs(models_dir, exist_ok=True)
     joblib.dump(best_model, os.path.join(models_dir, 'model.pkl'))
